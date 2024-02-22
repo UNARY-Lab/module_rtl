02565c9854f0c4aadd708bc431142a1f2d6a1166
Di Wu (Dee Woo) <diwu0111@hotmail.com>
2024-02-21 23:48:19 -0500

    add adder verification

--- module_rtl/adder/adder.v
+++ module_rtl/adder/adder.v
@@ -1,62 +1,20 @@
 `ifndef adder
 `define adder
 
-module adder #(
-    parameter BITWIDTH=32
-) (
+`include "adder.def"
+
+module adder (
     input wire iClk,
     input wire iRstN,
     input wire iEn,
     input wire iClr,
-    input wire [BITWIDTH-1 : 0] iData0,
-    input wire [BITWIDTH-1 : 0] iData1,
-    output reg [BITWIDTH : 0] oData
+    input wire [`BITWIDTH-1 : 0] iData0,
+    input wire [`BITWIDTH-1 : 0] iData1,
+    output reg [`BITWIDTH : 0] oData
 );
 
-    wire [BITWIDTH : 0] oSum
-    assign oSum = iData0 + iData1;
-
-    REGISTER #(
-        .BITWIDTH(BITWIDTH+1)
-    ) u_REGISTER(
-        .iClk(iClk),
-        .iRstN(iRstN),
-        .iEn(iEn),
-        .iClr(iClr),
-        .iData(oSum),
-        .oData(oData)
-    );
+    assign oData = iData0 + iData1;
     
-endmodule
-
-
-module REGISTER #(
-    parameter BITWIDTH=32
-) (
-    input wire iClk,
-    input wire iRstN,
-    input wire iEn,
-    input wire iClr,
-    input wire [BITWIDTH-1 : 0] iData,
-    output reg [BITWIDTH-1 : 0] oData
-);
-
-    always@(posedge iClk or negedge iRstN) begin
-        if (~iRstN) begin
-            oData <= 0;
-        end else begin
-            if (iClr) begin
-                oData <= 0;
-            end else begin
-                if (iEn) begin
-                    oData <= iData;
-                end else begin
-                    oData <= iData;
-                end
-            end
-        end
-    end
-
 endmodule
 
 `endif
---
+++ module_rtl/adder/adder_tb.sv
@@ -0,0 +1,43 @@
+`timescale 1ns/1ns
+
+`include "adder.v"
+
+module adder_tb ();
+
+    logic iClk;
+    logic iRstN;
+    logic iEn;
+    logic iClr;
+    logic [`BITWIDTH-1 : 0] iData0;
+    logic [`BITWIDTH-1 : 0] iData1;
+    logic [`BITWIDTH : 0] oData;
+
+    adder u_adder (
+        .iData0(iData0),
+        .iData1(iData1),
+        .oData(oData)
+    );
+
+    // clk define
+    always #5 iClk = ~iClk;
+
+    initial begin
+        $dumpfile("adder.vcd"); $dumpvars;
+    end
+
+    initial
+    begin
+        iClk = 1;
+        iRstN = 0;
+        iEn = 1;
+        iClr = 0;
+        iData0 = 'd10;
+        iData1 = 'd20;
+        
+        #15;
+        iRstN = 1;
+        #400;
+        $finish;
+    end
+
+endmodule
