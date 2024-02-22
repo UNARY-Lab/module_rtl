be8ae41d09a9aabdce2eac14cdf76acffcb58c34
Di Wu (Dee Woo) <diwu0111@hotmail.com>
2024-02-21 23:35:00 -0500

    add register verif

--- module_rtl/register/register.v
+++ module_rtl/register/register.v
@@ -1,15 +1,15 @@
 `ifndef register
 `define register
 
-module register #(
-    parameter BITWIDTH=32
-) (
+`include "register.def"
+
+module register (
     input wire iClk,
     input wire iRstN,
     input wire iEn,
     input wire iClr,
-    input wire [BITWIDTH-1 : 0] iData,
-    output reg [BITWIDTH-1 : 0] oData
+    input wire [`BITWIDTH-1 : 0] iData,
+    output reg [`BITWIDTH-1 : 0] oData
 );
 
     always@(posedge iClk or negedge iRstN) begin
---
+++ module_rtl/register/register_tb.sv
@@ -0,0 +1,54 @@
+`timescale 1ns/1ns
+
+`include "register.v"
+
+module register_tb ();
+
+    logic iClk;
+    logic iRstN;
+    logic iEn;
+    logic iClr;
+    logic [`BITWIDTH-1 : 0] iData;
+    logic [`BITWIDTH-1 : 0] oData;
+
+    register u_register (
+        .iClk(iClk),
+        .iRstN(iRstN),
+        .iEn(iEn),
+        .iClr(iClr),
+        .iData(iData),
+        .oData(oData)
+    );
+
+    // clk define
+    always #5 iClk = ~iClk;
+
+    initial begin
+        $dumpfile("cntwithen.vcd"); $dumpvars;
+    end
+
+    initial
+    begin
+        iClk = 1;
+        iRstN = 0;
+        iEn = 1;
+        iClr = 0;
+        iData = 0;
+        
+        #15;
+        iRstN = 1;
+        #10;
+        iData = 'd10;
+        #10;
+        iData = 'd100;
+        #10;
+        iData = 'd1000;
+        #10;
+        iData = 'd10000;
+        #10;
+        iClr = 1;
+        #400;
+        $finish;
+    end
+
+endmodule
