f4ad47dfddd9fa32ff65c4177a9d3ea87f1feffc
Di Wu (Dee Woo) <diwu0111@hotmail.com>
2024-02-22 10:59:11 -0500

    add mod_adder and verification

---
+++ module_rtl/mod_adder/mod_adder.v
@@ -0,0 +1,20 @@
+`ifndef adder
+`define adder
+
+`include "mod_adder.def"
+
+module mod_adder (
+    input wire [`BITWIDTH-1 : 0] iData0,
+    input wire [`BITWIDTH-1 : 0] iData1,
+    input wire [`BITWIDTH-1 : 0] iQ,
+    output reg [`BITWIDTH-1 : 0] oData
+);
+
+    wire [`BITWIDTH : 0] oSum;
+
+    assign oSum = iData0 + iData1;
+    assign oData = (oSum < iQ) ? oSum[`BITWIDTH-1 : 0] : (oSum - iQ);
+    
+endmodule
+
+`endif
